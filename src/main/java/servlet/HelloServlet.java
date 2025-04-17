bb017a87957a8ec6ee0bdbc01d023fe7a765e603
exe1379 <exe1379@gmail.com>
2025-04-17 16:05:10 +0800

    123123

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -15,8 +15,7 @@ public class HelloServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("Hello Servlet");  // 將字串回應給瀏覽器
-		resp.getWriter().print("Hello Servlet"); 
+		resp.getWriter().print("Hello Servlet");  // 將字串回應給瀏覽器...
 	}
 	
 }
